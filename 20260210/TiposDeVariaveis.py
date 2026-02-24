817b34128045ae2630f7fd4e86695a1b3b5062fa
EdyInstrutorEdn <ednilson.cipolla@escoladanuvem.org>
2026-02-24 19:50:21 -0300

    (WIP) Aula dia 10/02/2026

---
+++ 20260210/TiposDeVariaveis.py
@@ -0,0 +1,11 @@
+variavel = 10
+print(str(variavel) + " é do tipo " + str(type(variavel)))
+
+variavel = "Agora sou uma string"
+print(variavel+ " é do tipo " + str(type(variavel)))
+
+variavel = 3.14
+print(str(variavel)+ " é do tipo " + str(type(variavel)))
+
+variavel = True
+print(str(variavel)+ " é do tipo " + str(type(variavel)))
---
+++ 20260210/operacoesMatematicas.py
@@ -0,0 +1,49 @@
+num1 = 10
+num2 = 5
+resultado = num1 + num2
+print("O resultado de " + str(num1) + " + " + str(num2) + " é " + str(resultado))
+
+print("")
+resultado = num1 - num2
+print("O resultado de " + str(num1) + " - " + str(num2) + " é " + str(resultado))
+
+print("")
+resultado = num1 * num2
+print("O resultado de " + str(num1) + " * " + str(num2) + " é " + str(resultado))
+
+print("")
+resultado = num1 / num2
+print("O resultado de " + str(num1) + " / " + str(num2) + " é " + str(resultado))
+
+print("")
+num2 = 3
+resultado = num1 / num2
+print("O resultado de " + str(num1) + " / " + str(num2) + " é " + str(resultado))
+resultado = num1 // num2
+print("O resultado de " + str(num1) + " // " + str(num2) + " é " + str(resultado))
+resultado = num1 % num2
+print("O resultado de " + str(num1) + " % " + str(num2) + " é " + str(resultado))
+
+print("")
+num1 = 11
+num2 = 3
+resultado = num1 / num2
+print("O resultado de " + str(num1) + " / " + str(num2) + " é " + str(resultado))
+resultado = num1 // num2
+print("O resultado de " + str(num1) + " // " + str(num2) + " é " + str(resultado))
+resultado = num1 % num2
+print("O resultado de " + str(num1) + " % " + str(num2) + " é " + str(resultado))
+
+print("")
+num1 = 2
+num2 = 2
+resultado = num1 ** num2
+print("O resultado de " + str(num1) + " ** " + str(num2) + " é " + str(resultado))
+num1 = 2
+num2 = 3
+resultado = num1 ** num2
+print("O resultado de " + str(num1) + " ** " + str(num2) + " é " + str(resultado))
+num1 = 2
+num2 = 4
+resultado = num1 ** num2
+print("O resultado de " + str(num1) + " ** " + str(num2) + " é " + str(resultado))
