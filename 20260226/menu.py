cc35cbab9f519e167b235f9fecda314a007fccad
Allysson.Anacleto <allyssonanacleto@gmail.com>
2026-02-26 21:25:28 -0300

    Subindo funcionalidade 4

--- 20260226/menu.py
+++ 20260226/menu.py
@@ -1,26 +1,74 @@
 import os
 import maioridade
 
+
 def Opcao1():
     os.system('cls' if os.name == 'nt' else 'clear')
     print("============================")
     print("Classificação por idade")
     print("============================")
-    classification = int(input("Digite a sua idade: "))
-    result = maioridade.Maioridade(classification)
-    print("")
-    print(f"Sua faixa etária é: {result}")
+
+    try:
+        classification = int(input("Digite a sua idade: "))
+
+        if classification < 0:
+            print("Idade não pode ser negativa.")
+        else:
+            result = maioridade.Maioridade(classification)
+            print("")
+            print(f"Sua faixa etária é: {result}")
+
+    except ValueError:
+        print("Por favor, digite apenas números válidos.")
+
     print("")
     input("Pressione Enter para continuar...")
+
+
+def Opcao4():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("============================")
+    print("Classificação por experiência")
+    print("============================")
+
+    try:
+        anos = int(input("Digite seus anos de experiência: "))
+
+        if anos < 0:
+            print("O tempo de experiência não pode ser negativo.")
+        else:
+            niveis = {
+                range(0, 2): "Júnior",
+                range(2, 5): "Pleno",
+                range(5, 100): "Sênior"
+            }
+
+            resultado = "Nível não definido"
+
+            for faixa, nivel in niveis.items():
+                if anos in faixa:
+                    resultado = nivel
+                    break
+
+            print("")
+            print(f"Seu nível de experiência é: {resultado}")
+
+    except ValueError:
+        print("Por favor, digite apenas números válidos.")
+
+    print("")
+    input("Pressione Enter para continuar...")
+
 
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
     print("===== Menu Principal =====")
-    print("1. Mairoridade")
+    print("1. Maioridade")
+    print("4. Nível de Experiência")
     print("0. Exit")
     print("==========================")
+
     choice = input("Enter your choice: ")
-    
 
     try:
         choice = int(choice)
@@ -32,6 +80,8 @@ while True:
 
     if choice == 1:
         Opcao1()
+    elif choice == 4:
+        Opcao4()
     elif choice == 0:
         os.system('cls' if os.name == 'nt' else 'clear')
         print("Obrigado por usar o programa.")
@@ -40,4 +90,3 @@ while True:
         print("Opção inválida. Por favor, tente novamente.")
         print("")
         input("Pressione Enter para continuar...")
-        
